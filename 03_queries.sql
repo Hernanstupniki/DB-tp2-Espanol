9c8ad06b3fccfe2e930d55adbe23f75282742e20
Stupniki Hernán <hernanst7@gmail.com>
2025-10-16 17:16:58 -0300

    Update 03_queries.sql

--- 03_queries.sql
+++ 03_queries.sql
@@ -125,12 +125,6 @@ LEFT JOIN solicitudesviaje s
 WHERE c.estado = 'conectado'
   AND s.id IS NULL;
 
-SELECT id, nombre, apellido
-FROM conductoresactivos
-WHERE ST_Within(
-  ubicacion,
-  ST_GeomFromText('POLYGON((-55.905 -27.368,-55.895 -27.368,-55.895 -27.362,-55.905 -27.362,-55.905 -27.368))',4326)
-);
 
 -- Vista
 SELECT
